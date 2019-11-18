c7515b53a1198983a4f926f4b6c74023719cb622
PND <fujjisaaan@gmail.com>
2019-11-18 23:41:17 +0900

    [fix] Fix sizes of the icons.

--- resize.sh
+++ resize.sh
@@ -2,5 +2,5 @@
 
 for size in 16 32 48 128
 do
-  sips -Z ${size} resources/icon.png --out src/images/icon${size}.png
+  sips -z ${size} ${size} resources/icon.png --out src/images/icon${size}.png
 done
